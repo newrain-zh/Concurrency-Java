f75834de337c80c6d86ec6a099b1af40472b04e1
newrain-zh <496268609@qq.com>
2022-10-31 22:43:10 +0800

    feat(all): 类名、注释、日志修改

--- src/main/java/com/newrain/concurrency/utils/semaphore/CountDownLatchExample.java
+++ src/main/java/com/newrain/concurrency/utils/semaphore/CountDownLatchExample.java
@@ -1,24 +1,30 @@
 package com.newrain.concurrency.utils.semaphore;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.CountDownLatch;
 
+/**
+ * @author newRain
+ */
+@Slf4j
 public class CountDownLatchExample {
 
 
     public static void main(String[] args) {
-        CountDownLatch countDownLatch = new CountDownLatch(10);
+
+        CountDownLatch countDownLatch = new CountDownLatch(100);
         //模拟100辆车进入停车场
         for (int i = 0; i < 100; i++) {
             Thread thread = new Thread(() -> {
                 try {
-                    System.out.println("====" + Thread.currentThread().getName() + "来到停车场");
-                    System.out.println(Thread.currentThread().getName() + "成功进入停车场");
-//                    Thread.sleep(new Random().nextInt(10000));//模拟车辆在停车场停留的时间
+                    log.debug("{}:来到停车场", Thread.currentThread().getName());
+                    log.debug("{}: 成功进入停车场", Thread.currentThread().getName());
+                    countDownLatch.countDown();
                     Thread.sleep(10000);//模拟车辆在停车场停留的时间
-                    System.out.println(Thread.currentThread().getName() + "驶出停车场");
-                    countDownLatch.countDown();//释放令牌，腾出停车场车位
+                    log.debug("{}:驶出停车场", Thread.currentThread().getName());
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    log.error("error", e);
                 }
             }, i + "号车");
             thread.start();
--- src/main/java/com/newrain/concurrency/utils/semaphore/SemaphoreExample.java
+++ src/main/java/com/newrain/concurrency/utils/semaphore/SemaphoreExample.java
@@ -1,32 +1,35 @@
 package com.newrain.concurrency.utils.semaphore;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.Random;
 import java.util.concurrent.Semaphore;
 
+/**
+ * @author newRain
+ * @description Semaphore使用示例
+ */
+@Slf4j
 public class SemaphoreExample {
-    private static Semaphore semaphore = new Semaphore(10, true);
+    private static final Semaphore SEMAPHORE = new Semaphore(10, true);
 
     public static void main(String[] args) {
         //模拟100辆车进入停车场
         for (int i = 0; i < 100; i++) {
             Thread thread = new Thread(() -> {
                 try {
-                    System.out.println("====" + Thread.currentThread().getName() + "来到停车场");
-                    if (semaphore.availablePermits() == 0) {
-                        System.out.println("车位不足，请耐心等待");
-                    }
-                        semaphore.acquire();//获取令牌尝试进入停车场
-                    System.out.println(Thread.currentThread().getName() + "成功进入停车场");
-                    Thread.sleep(new Random().nextInt(10000));//模拟车辆在停车场停留的时间
-                    System.out.println(Thread.currentThread().getName() + "驶出停车场");
-                    semaphore.release();//释放令牌，腾出停车场车位
+                    log.debug("{}:来到停车场", Thread.currentThread().getName());
+                    SEMAPHORE.acquire();//获取令牌尝试进入停车场
+                    log.debug("{}: 成功进入停车场", Thread.currentThread().getName());
+                    Thread.sleep(5000);//模拟车辆在停车场停留的时间
+                    SEMAPHORE.release();//释放令牌，腾出停车场车位
+                    log.debug("{}:驶出停车场", Thread.currentThread().getName());
                 } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    log.error("error:", e);
                 }
             }, i + "号车");
             thread.start();
         }
-
-        System.out.println("主线程完成");
+        log.debug("主线程完成");
     }
 }
