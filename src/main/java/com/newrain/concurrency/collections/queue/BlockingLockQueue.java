741b68aea5bbc2ff5a91fccdcea8d51b3dbb7b5d
alex <zzqnobody@126.com>
2023-09-22 18:56:56 +0800

    阻塞队列实现

--- src/main/java/com/newrain/concurrency/collections/queue/BlockingLockQueue.java
+++ src/main/java/com/newrain/concurrency/collections/queue/BlockingLockQueue.java
@@ -1,5 +1,8 @@
 package com.newrain.concurrency.collections.queue;
 
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.LinkedList;
 import java.util.List;
 import java.util.concurrent.locks.Condition;
@@ -7,23 +10,22 @@ import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
+ *
+ * ReentrantLock 实现阻塞队列
  * @author newRain
  * @description 阻塞队列示例
  */
-public class BlockingLockQueue {
+@Slf4j
+public class BlockingLockQueue<T> {
 
     final Lock lock = new ReentrantLock();
     final Condition notFull = lock.newCondition();
     final Condition notEmpty = lock.newCondition();
 
-    private List list;
+    private final List<T> list;
 
+    @Getter
     private int limit;
-
-    public int getLimit() {
-
-        return limit;
-    }
 
     public void setLimit(int limit) {
         this.limit = limit;
@@ -31,16 +33,16 @@ public class BlockingLockQueue {
 
     public BlockingLockQueue(int limit) {
         this.limit = limit;
-        list = new LinkedList();
+        list = new LinkedList<>();
     }
 
 
-    public void enqueue(Object item) throws InterruptedException {
+    public void enqueue(T item) throws InterruptedException {
         lock.lock();
         while (this.list.size() == limit) {
             notFull.await();
         }
-        while (this.list.size() == 0) {
+        while (this.list.isEmpty()) {
             notEmpty.signal();
         }
         list.add(item);
@@ -49,7 +51,7 @@ public class BlockingLockQueue {
 
     public void dequeue() throws InterruptedException {
         lock.lock();
-        while (this.list.size() == 0) {
+        while (this.list.isEmpty()) {
             notEmpty.await();
         }
 
