5326e23ac1018a2c3ce7d7066ce71b757a02419f
alex <zzqnobody@126.com>
2023-09-26 14:58:14 +0800

    缓存行性能测试

--- src/main/java/com/newrain/concurrency/os/cacheline/CacheLineTest.java
+++ src/main/java/com/newrain/concurrency/os/cacheline/CacheLineTest.java
@@ -1,12 +1,15 @@
 package com.newrain.concurrency.os.cacheline;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author newRain
  * @description 缓存行性能测试
  */
+@Slf4j
 public class CacheLineTest {
-    static final int LINE_NUM = 2048;
-    static final int COLUM_NUM = 2048;
+    static final int LINE_NUM = 8192;
+    static final int COLUM_NUM = 8192;
 
     public static void main(String[] args) {
         test1();
@@ -25,7 +28,7 @@ public class CacheLineTest {
             }
         }
         long end = System.currentTimeMillis();
-        System.out.println("test1 cache time:" + (end - start));
+        log.info("顺序执行 cache time={}毫秒", end - start);
     }
 
     /**
@@ -40,7 +43,7 @@ public class CacheLineTest {
             }
         }
         long end = System.currentTimeMillis();
-        System.out.println("test2 cache time:" + (end - start));
+        log.info("乱序执行 cache time={}毫秒", end - start);
     }
 
 }
